46946d28a3b7d2620bb0a3f815959c6302095b46
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-26 19:48:54 -0300

    chore: separate components and added new button filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
 
 export function Orders() {
     return (
@@ -12,10 +12,7 @@ export function Orders() {
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
             </div>
             <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input className="h-8 w-[320px]" placeholder="Nome do cliente"/>
-                </form>
+               <OrderTableFilters />
                 <div className="border rounded-md">
                     <Table>
                         <TableHeader>
@@ -32,46 +29,7 @@ export function Orders() {
                         </TableHeader>
                         <TableBody>
                             {Array.from({length: 10}).map((_, i) => (
-                                <TableRow key={i}>
-                                    <TableCell>
-                                        <Button variant="outline" size="xs">
-                                            <Search className="h-3 w-3">
-                                                <span className="sr-only">Detalhes do pedido</span>
-                                            </Search>
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell className="font-mono text-xs font-medium">
-                                        0a9s08ea90s831
-                                    </TableCell>
-                                    <TableCell className="text-muted-foreground">
-                                        há 15 minutos
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className="flex items-center gap-2">
-                                            <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                            <span className="font-medium text-muted-foreground">Pendente</span>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="font-medium">
-                                        Fellipe Emanoel
-                                    </TableCell>
-                                    <TableCell className="font-medium">
-                                        R$ 100, 00
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant="outline" size="xs">
-                                            <ArrowRight className="h-3 w-3 mr-2"/>
-                                            Aprovar
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant="ghost" size="xs">
-                                            <X className="h-3 w-3 mr-2"/>
-                                            Cancelar
-                                        </Button>
-                                    </TableCell>
-                                
-                                </TableRow>
+                               <OrderTableRow key={i}/>
                             ))}
                         </TableBody>
                     </Table>
