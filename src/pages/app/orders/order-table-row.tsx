e39b8305e8141f8730ee8a11b1b07c3f3b038543
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-26 20:32:35 -0300

    feature: create order detail dialog

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
+
 import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
     return (
          <TableRow>
             <TableCell>
-                <Button variant="outline" size="xs">
-                    <Search className="h-3 w-3">
-                        <span className="sr-only">Detalhes do pedido</span>
-                    </Search>
-                </Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" size="xs">
+                            <Search className="h-3 w-3">
+                                <span className="sr-only">Detalhes do pedido</span>
+                            </Search>
+                        </Button>
+                    </DialogTrigger>
+                    <OrderDetails />
+                </Dialog>
+                
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
                 0a9s08ea90s831
