a5510b30200b41e4129ebb5bbee1f3d9c17815f2
fellipesnoww <fellipe2.0e@gmail.com>
2025-08-30 18:52:26 -0300

    config: add libraries to execute tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite"
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +14,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
+  }
+} as UserConfig & {
+  test: InlineConfig
 })
